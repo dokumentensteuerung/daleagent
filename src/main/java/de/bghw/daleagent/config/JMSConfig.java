63dc253dfa3d59b3270887dee547afd57ae58efb
xf0e <droidlove@ya.ru>
2020-11-07 07:11:41 +0100

    [BUGFIX] resolved warnings for JmsComponent declarations, removed unneeded deps, added rabbitmq to pom

--- src/main/java/de/bghw/daleagent/config/JMSConfig.java
+++ src/main/java/de/bghw/daleagent/config/JMSConfig.java
@@ -4,6 +4,7 @@ import javax.jms.ConnectionFactory;
 
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +14,14 @@ import org.springframework.jms.connection.JmsTransactionManager;
 public class JMSConfig {
 
     @Bean
-    public JmsTransactionManager jmsTransactionManager(final ConnectionFactory connectionFactory) {
+    public JmsTransactionManager jmsTransactionManager(@Qualifier("jmsConnectionFactory") final ConnectionFactory connectionFactory) {
         JmsTransactionManager jmsTransactionManager = new JmsTransactionManager();
         jmsTransactionManager.setConnectionFactory(connectionFactory);
         return jmsTransactionManager;
     }
    
     @Bean
-    public JmsComponent jmsComponent(final ConnectionFactory connectionFactory, final JmsTransactionManager jmsTransactionManager) {
+    public JmsComponent jmsComponent(@Qualifier("jmsConnectionFactory") final ConnectionFactory connectionFactory, final JmsTransactionManager jmsTransactionManager) {
         JmsComponent jmsComponent = JmsComponent.jmsComponentTransacted(connectionFactory, jmsTransactionManager);
         return jmsComponent;
     }
