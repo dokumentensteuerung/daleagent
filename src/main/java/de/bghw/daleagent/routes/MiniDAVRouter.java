4423a3c7d487bd5f8f081020805a338497f01c3c
Eugen Buch <eugen.buchmueller@gmail.com>
2020-11-08 22:28:50 +0100

    play with camel

--- src/main/java/de/bghw/daleagent/routes/MiniDAVRouter.java
+++ src/main/java/de/bghw/daleagent/routes/MiniDAVRouter.java
@@ -1,5 +1,7 @@
 package de.bghw.daleagent.routes;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.zipkin.ZipkinTracer;
 import org.springframework.stereotype.Component;
@@ -16,7 +18,7 @@ public class MiniDAVRouter extends RouteBuilder {
 		zipkin.setServiceName("MiniDAVRouter");
 		// capture 100% of all the events
 		zipkin.setRate(1.0f);
-		
+
 		// include message bodies in the traces (not recommended for production)
 		zipkin.setIncludeMessageBodyStreams(true);
 		// add zipkin to CamelContext
@@ -27,27 +29,28 @@ public class MiniDAVRouter extends RouteBuilder {
 	public void configure() throws Exception {
 		// Zipkin zum Camelcontext verbinden
 		configureZipkin();
-	    
-	    // Route 1 :Dokumente einlesen und in Queue ablegen
-//		from("file:input?delay=1000").id("minidav").
-//		process(new Processor() {
-//			@Override
-//			public void process(Exchange exchange) throws Exception {
-//				System.out.println("Dokument angenommen");
-//			}
-//		}).
-//		to("file:output").to("jms:queue:daledokument?requestTimeout=30s");
-//		
-//
-//		// Route 2: Dokumente aus der Queue holen, verarbeiten
-//		from("jms:queue:daledokument").
-//		process(new Processor() {
-//			@Override
-//			public void process(Exchange exchange) throws Exception {
-//				System.out.println("Dokumentverarbeitung gestartet");
-//			}
-//		});
-		
+
+		// Tracing aktivieren
+		getContext().setTracing(true);
+
+		// Exceptionhandling
+		onException(Exception.class).handled(true);
+
+		// Route 1 :Dokumente einlesen und in Queue ablegeN
+		from("file:input?delay=1000&readLock=changed&noop=true").id("minidav").choice().when()
+				.simple("${file:name.ext} == 'xml'").log("Verarbeite Datei: ${file:name}").to("file:output")
+				.to("jms:queue:daledokument?requestTimeout=30s").to("direct:xml");
+
+		from("direct:xml").routeId("XXX").log("processing xml");
+
+		// Route 2: Dokumente aus der Queue holen, verarbeiten
+		from("jms:queue:daledokument").process(new Processor() {
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				System.out.println("Dokumentverarbeitung gestartet");
+			}
+		});
+
 	}
 
 }
